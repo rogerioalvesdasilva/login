668b712ef7dd481abf64e48158f1c53dbf9df282
Rogério Alves da Silva <60400314+rogerioalvesdasilva@users.noreply.github.com>
2020-11-28 14:18:46 -0300

    add-arquivos-28-11-20
    Adicionando o index, login.php e login.css

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Login</title>
+</head>
+<body>
+  
+    <div class="container">
+        <div class="box">
+            <div class="area-header">
+                <div class="image-box" >Image</div>
+                <div class="header-txt">Sua Empresa </div>
+            </div>
+            <div class="area-login">
+                <form action="" method="post">
+                    <input class="input-box" type="text" id="login" name="login" placeholder="Digite seu login"><br>
+                    <input class="input-box" type="password" id="password" name="password" placeholder="Digite sua senha"><br>
+                    <input class="btn-login" type="submit" value="Entrar">
+                </form>
+            </div>
+            <div class="link-conta"><p class="txt">Não é registrado? <a href="#">crie uma conta</a> </p></div>
+        </div>
+    </div>
+</body>
+</html>
